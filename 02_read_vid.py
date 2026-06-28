ee2210830ba666c69733590344a8e53d09787b13
zal <zalea473@gmail.com>
2026-06-28 13:58:59 +0700

    fix: update video playback controls and add comments for clarity in 02_read_vid.py

--- 02_read_vid.py
+++ 02_read_vid.py
@@ -2,12 +2,12 @@ import cv2 as cv
 
 capture = cv.VideoCapture('vid/cat.mp4')
 
-while True:
-    isTrue, frame = capture.read()
+while True: # need to use while loop to read the video frame by frame
+    isTrue, frame = capture.read() # isTrue is a boolean value, frame is the actual frame of the video, and capture.read() reads the video frame by frame
     cv.imshow('Video', frame)
 
-    if cv.waitKey(20) & 0xFF == ord('d'):
+    if cv.waitKey(1) & 0xFF == ord('q'):  # means if the user presses the 'q' key, then break the loop and stop the video
         break
 
-capture.release()
-cv.destroyAllWindows()  
+capture.release() # if we don't release the video capture, then the video will not be released and will keep running in the background that causes a memory leak and will slow down the computer
+cv.destroyAllWindows()  # might freeze the computer if we don't destroy all the windows that are created by OpenCV
